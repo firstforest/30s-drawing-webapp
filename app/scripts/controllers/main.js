37da03345ebe8f09daa8bd57ba898c42a91e8073
firstforest <h.morikosi@gmail.com>
2014-02-15 21:36:28 +0900

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('30sDrawingWebappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
